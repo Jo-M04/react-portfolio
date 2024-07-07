56f9e9ff8b37a4be2da42eb67b8c50e6fa718497
Jo-M04 <joseph.i.miller004@gmail.com>
2024-07-06 20:24:00 -0500

    added navbar and background

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import Navbar from './components/navbar'
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-    Hello world!</h1>
+      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <Navbar/>
   </div>
   )
 }
